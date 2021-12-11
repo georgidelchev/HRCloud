0d51cebb4bbb7b2b590e7d5d7a5fee415a041d2c
george <alienguymc@gmail.com>
2021-12-11 13:59:03 +0200

    add template

---
+++ Data/HRCloud.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace HRCloud.Data.Models
+{
+    using HRCloud.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
