a679f8970d65affeaadf2606ed07c605c972e93d
george <alienguymc@gmail.com>
2021-12-25 10:52:24 +0200

    add administration authorization to all controllers

--- Web/HRCloud.Web/Controllers/BaseController.cs
+++ Web/HRCloud.Web/Controllers/BaseController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HRCloud.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HRCloud.Web.Controllers
 {
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     public class BaseController : Controller
     {
     }
